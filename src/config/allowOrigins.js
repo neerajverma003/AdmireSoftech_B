2d9cd7dcb675ece92204493fcf55cbb6282666fe
kaif <mohdkaif8672@gmail.com>
2026-08-25 12:53:17 +0530

    update env

--- src/config/allowOrigins.js
+++ src/config/allowOrigins.js
@@ -2,11 +2,17 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:3000",
+  "http://127.0.0.1:5173",
+  "http://127.0.0.1:5174",
+  "http://127.0.0.1:3000",
   "http://admiresoftech.com",
   "https://admiresoftech.com",
   "http://www.admiresoftech.com",
   "https://www.admiresoftech.com",
+  "http://admin.admiresoftech.com",
+  "https://admin.admiresoftech.com",
+  "http://api.admiresoftech.com",
+  "https://api.admiresoftech.com",
 ];
 
 export default allowedOrigins;
-
