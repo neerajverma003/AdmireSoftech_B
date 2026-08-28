52e6cfa789dbbc08a8908c959e7798d35332bd79
Neeraj Verma <Neerajverma1663@gmail.com>
2026-08-28 16:32:41 +0530

    update

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -11,7 +11,7 @@ const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
   sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days 
 };
 
 
